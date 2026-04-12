85d94159700e3cd7660396c318c46c6a1e6e77d6
kyoulau <laucosan05@gmail.com>
2026-04-12 01:39:42 -0300

    feat user register

---
+++ src/common/filter/database-exception.filter.ts
@@ -0,0 +1,28 @@
+// Tratamento para e-mail duplicado no PostgreSQL
+import { ExceptionFilter, Catch, ArgumentsHost, HttpStatus } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { QueryFailedError } from 'typeorm';
+
+@Catch(QueryFailedError)
+export class DatabaseExceptionFilter implements ExceptionFilter {
+  catch(exception: QueryFailedError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    if ((exception as any).code === '23505') {
+      return response.status(HttpStatus.CONFLICT).json({
+        statusCode: HttpStatus.CONFLICT,
+        message: 'Os dados informados já existem no sistema. (Ex: e-mail já cadastrado)',
+        error: 'Conflict',
+        path: request.url,
+      });
+    }
+
+    response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Erro interno no banco de dados.',
+      path: request.url,
+    });
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,26 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Usuario } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(Usuario)
+    private usuariosRepository: Repository<Usuario>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+      const saltRounds = 10;
+      const hash = await bcrypt.hash(createUserDto.password, saltRounds);
+
+      const novoUsuario = this.usuariosRepository.create({
+      nome: createUserDto.nome,
+      email: createUserDto.email,
+      passwordHash: hash,
+    });
+
+    const usuarioSalvo = await this.usuariosRepository.save(novoUsuario);
+
+    const { passwordHash, ...userSemSenha } = usuarioSalvo;
+    return userSemSenha;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async createViaGoogle(nome: string, email: string, googleId: string) {
+    const usuarioExistente = await this.usuariosRepository.findOne({ where: { email } });
+
+    if (usuarioExistente) {
+      if (!usuarioExistente.googleId) {
+        usuarioExistente.googleId = googleId;
+        await this.usuariosRepository.save(usuarioExistente);
+      }
+      return usuarioExistente;
+    }
+
+    const novoUsuarioGoogle = this.usuariosRepository.create({
+      nome,
+      email,
+      googleId,
+    });
+
+    return await this.usuariosRepository.save(novoUsuarioGoogle);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+
+  async findOne(id: string ) {
+    const usuario = await this.usuariosRepository.findOne({ where: { id } });
+    
+    if (!usuario) {
+      throw new NotFoundException('Usuário não encontrado em nossa base de dados.');
+    }
+    
+    return usuario
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const usuario = await this.findOne(id);
+
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+      usuario.passwordHash = updateUserDto.password;
+    }
+
+    if (updateUserDto.nome) usuario.nome = updateUserDto.nome;
+    if (updateUserDto.email) usuario.email = updateUserDto.email;
+
+    const usuarioAtualizado = await this.usuariosRepository.save(usuario);
+
+    const { passwordHash, ...userSemSenhaAtualizado } = usuarioAtualizado;
+    
+    return userSemSenhaAtualizado;
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    await this.findOne(id);
+    
+    await this.usuariosRepository.softDelete(id);
+    
+    return { message: 'Usuário excluído com sucesso.' };
   }
 }
